936f0a30f6876db79cf325cbf9a028b63d57b69c
Guilherme Alves Pimenta <guialvespimenta27@gmail.com>
2022-07-14 21:54:28 -0300

    geolocaliation feature (need improvements)

--- screen/Driving/Map.jsx
+++ screen/Driving/Map.jsx
@@ -1,32 +1,24 @@
 import * as React from "react";
 
 import {
+  Accuracy,
   getCurrentPositionAsync,
-  useForegroundPermissions,
   PermissionStatus,
+  useForegroundPermissions,
+  watchPositionAsync,
 } from "expo-location";
 
-import {
-  StyleSheet,
-  Text,
-  View,
-  Dimensions,
-  Button,
-  Alert,
-  Image,
-} from "react-native";
-import MapView from "react-native-maps";
-
+import { Alert, Button, Dimensions, StyleSheet, View } from "react-native";
 
 export default function App() {
   const [locationPicked, setLocationPicked] = React.useState();
-  const [locationPermissionInformation, requestPermission] =
-    useForegroundPermissions();
+  const [locationPermissionInformation, requestPermission] = useForegroundPermissions();
+  const [endPoint, setEndPoint] = React.useState({ lat: -22.9094734, lng: -43.1080839 });
+  const [actualLocation, setActualLocation] = React.useState({});
+  const [speed, setSpeed] = React.useState();
 
   async function VerifyPermissions() {
-    if (
-      locationPermissionInformation.status === PermissionStatus.UNDETERMINED
-    ) {
+    if (locationPermissionInformation.status === PermissionStatus.UNDETERMINED) {
       const PermissionResponse = await requestPermission();
       return PermissionResponse;
     }
@@ -34,38 +26,52 @@ export default function App() {
       Alert.alert("Insuficient Permissions", "You need to grant permission");
       return false;
     }
-    return true
+    return true;
   }
-  async function getLocationHandler() {
-      const hasPermission = await VerifyPermissions();
-      if(!hasPermission){
-          return
-        }
-        const location = await getCurrentPositionAsync();
-        setLocationPicked({
+  async function watchPosition() {
+    const hasPermission = await VerifyPermissions();
+    if (!hasPermission) {
+      return;
+    }
+    const location = await getCurrentPositionAsync();
+    setActualLocation({
+      lat: location.coords.latitude,
+      lng: location.coords.longitude,
+    });
+    let locationSubscrition = null;
+    while (actualLocation.lat !== endPoint.lat && actualLocation.lng !== endPoint.lng) {
+      locationSubscrition = await watchPositionAsync(
+        {
+          accuracy: Accuracy.BestForNavigation,
+          distanceInterval: 1,
+          timeInterval: 1000,
+        },
+        (location) => {
+          setActualLocation({
             lat: location.coords.latitude,
             lng: location.coords.longitude,
-        });
-    
-    console.log(location.coords.latitude);
-    console.log(location.coords.longitude);
+          });
+          setSpeed(location.coords.speed);
+        }
+      );
+    }
+    locationSubscrition?.remove();
+    setSpeed(0);
   }
 
   return (
     <View style={styles.container}>
-       
-      <Button onPress={getLocationHandler} title='take current location' />
+      <Button onPress={watchPosition} title="take current location" />
     </View>
-  )
+  );
 }
 const styles = StyleSheet.create({
-    container:{       
-        width: '100%',
-        justifyContent:'flex-end'
-    }
-    ,
-  map: {
-    width: Dimensions.get('window').width,
-    height: Dimensions.get('window').height,
+  container: {
+    width: "100%",
+    justifyContent: "flex-end",
   },
-})
+  map: {
+    width: Dimensions.get("window").width,
+    height: Dimensions.get("window").height,
+  },
+});
